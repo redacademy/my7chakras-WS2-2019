5a86d42cae611920de109360c7fc3e75a41e54f3
Diego <diegobustos1229@gmail.com>
2020-02-26 18:46:37 -0800

    subscribe to nst

--- themes/my7chakras/footer.php
+++ themes/my7chakras/footer.php
@@ -12,6 +12,7 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div> Subscribe to Newsletter</div>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
