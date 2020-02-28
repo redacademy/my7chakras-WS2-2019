14505bb6456685eb4bcd85b19c96eb46bd1fad13
Mayu Imanaka <imamayu.1215@gmail.com>
2020-02-27 19:28:07 -0800

    add:_episodes.scss,update:font style

--- themes/my7chakras/home.php
+++ themes/my7chakras/home.php
@@ -8,26 +8,34 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="content-area episodes">
     <main id="main" class="site-main" role="main">
+        <div class="episodes__wrapper">
+            <h1>My Seven Chakras Podcast Episode</h1>
 
-        <?php if (have_posts()) : ?>
+            <h2>Categories</h2>
 
-            <?php if (is_home() && !is_front_page()) : ?>
-                <header>
-                    <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-                </header>
+            <h2>Episodes</h2>
+
+            <?php if (have_posts()) : ?>
+
+                <?php if (is_home() && !is_front_page()) : ?>
+                    <header>
+                        <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                    </header>
+                <?php endif; ?>
+                <?php while (have_posts()) : the_post(); ?>
+
+
+
+                <?php endwhile; ?>
+
+            <?php else : ?>
+
+                <?php get_template_part('template-parts/content', 'none'); ?>
+
             <?php endif; ?>
-
-
-            <?php the_posts_navigation(); ?>
-
-        <?php else : ?>
-
-            <?php get_template_part('template-parts/content', 'none'); ?>
-
-        <?php endif; ?>
-
+        </div>
     </main><!-- #main -->
 </div><!-- #primary -->
 
