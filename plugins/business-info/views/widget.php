7a2de44b5278dc70779ad1014c3602c78e6e1271
Diego <diegobustos1229@gmail.com>
2020-02-29 14:57:31 -0800

    add: footer desktop and phone view

--- plugins/business-info/views/widget.php
+++ plugins/business-info/views/widget.php
@@ -1,27 +1,29 @@
 <!-- This file is used to markup the public-facing widget. -->
 
-
-<P>
-    <?php if (strlen(trim($instagram))>0): ?>
-        <a href="https://www.instagram.com/<?php echo $instagram; ?>">
-        <img src= "<?php echo get_template_directory_uri();?>/assets/logo/IG.svg" alt="Instagram logo text">				
+<div class="box-row">
 
 
+    
+    <P>
+        <?php if (strlen(trim($facebook))>0): ?>
+            <a href="https://www.facebook.com/<?php echo $facebook; ?>">
+            
+            <img src= "<?php echo get_template_directory_uri();?>/assets/logo/FB.svg" alt="Inhabitent logo text">				
+            
         </a>
-       
-
-    <?php endif; ?>
-</P>
-
-<P>
-    <?php if (strlen(trim($facebook))>0): ?>
-        <a href="https://www.facebook.com/<?php echo $facebook; ?>">
         
-        <img src= "<?php echo get_template_directory_uri();?>/assets/logo/FB.svg" alt="Inhabitent logo text">				
+        <?php endif; ?>
+    </P>
 
+    <P>
+        <?php if (strlen(trim($instagram))>0): ?>
+            <a href="https://www.instagram.com/<?php echo $instagram; ?>">
+            <img src= "<?php echo get_template_directory_uri();?>/assets/logo/IG.svg" alt="Instagram logo text">				
         </a>
-      
-    <?php endif; ?>
-</P>
-
+        
+        
+        <?php endif; ?>
+    </P>
+    
+</div>
 
