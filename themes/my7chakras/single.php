f4284be7ac450f92dc92dc80603b8232c39164f4
Mayu Imanaka <imamayu.1215@gmail.com>
2020-03-04 20:44:56 -0800

    add:background gradient style to single.php

--- themes/my7chakras/single.php
+++ themes/my7chakras/single.php
@@ -22,35 +22,37 @@ get_header(); ?>
 
 			?>
 
-			<article id="post-<?php the_ID(); ?>" class="blog__post">
-				<div class="blog__postWrapper">
+			<article id="post-<?php the_ID(); ?>" class="post">
+				<div class="post__Wrapper">
 					<p>Episode <?php echo getPostThNumber() ?></p>
 					<?php echo get_the_date('F d'); ?>
-					<?php the_title('<h3 class="blog__postTitle">', '</h3>'); ?>
-					<p class="blog__postTag">
+					<?php the_title('<h3 class="post__Title">', '</h3>'); ?>
+					<p class="post__tag">
 						<?php the_tags('', ' | '); ?>
 					</p>
-					<div class="blog__postThumbnail">
+					<div class="post__thumbnail">
 						<?php if (has_post_thumbnail()) : ?>
 							<?php the_post_thumbnail('large'); ?>
 						<?php endif; ?>
 					</div>
-
-
 					<?php the_content(); ?>
 				</div>
-				<div class="blog__postText episode__question">
-					<h3>Featured Questions <span class="">?</span><span>:</span></h3>
-					<ol class="">
-						<?php
-						if (post_custom('featured_questions')) {
-							$items = explode("\n", post_custom('featured_questions'));
-							foreach ($items as $value) {
-								echo '<li>' . $value . '</li>';
+				<div class="post__text">
+					<p class="post__white post__white--top"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/blog/white_top.svg" alt=""></p>
+					<div class="episode__question">
+						<h3>Featured Questions <span class="question">?</span><span class="colon">:</span></h3>
+						<ol class="">
+							<?php
+							if (post_custom('featured_questions')) {
+								$items = explode("\n", post_custom('featured_questions'));
+								foreach ($items as $value) {
+									echo '<li>' . $value . '</li>';
+								}
 							}
-						}
-						?>
-					</ol>
+							?>
+						</ol>
+					</div>
+					<p class="post__white post__white--bottom"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/blog/white_bottom.svg" alt=""></p>
 				</div>
 				<?php
 				wp_link_pages(array(
