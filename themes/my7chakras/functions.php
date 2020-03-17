c60c23d196f9785c9449a226471ce4afedbba510
Mayu Imanaka <imamayu.1215@gmail.com>
2020-03-16 19:49:43 -0700

    add:audio features

--- themes/my7chakras/functions.php
+++ themes/my7chakras/functions.php
@@ -106,6 +106,7 @@ function red_starter_scripts()
 	wp_enqueue_script('slick-js', get_template_directory_uri() . '/build/js/slick.min.js');
 	wp_enqueue_script('normal-js', get_template_directory_uri() . '/build/js/index.min.js');
 	wp_enqueue_script('slider-js', get_template_directory_uri() . '/build/js/slider.min.js');
+	wp_enqueue_script('audio-js', get_template_directory_uri() . '/build/js/audio.min.js');
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
